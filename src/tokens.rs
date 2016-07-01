a8a88a580f80b9e3a26fb57b3de1e452c2fcadb5
silverhes <17355260+silverhes@users.noreply.github.com>
2016-07-01 18:53:00 +0700

    refactor: tidy llm tooling

--- src/tokens.rs
+++ src/tokens.rs
@@ -9,3 +9,12 @@
 //! The estimator is a pure function of the input bytes, so two runs over the
 //! same file always agree.
 
+/// Estimate the token mass of a UTF-8 string.
+///
+/// The heuristic counts *segments*: maximal runs of alphanumeric characters are
+/// one segment each but long identifiers are split every four characters (BPE
+/// tends to fragment `snake_case_identifiers`), and each punctuation or symbol
+/// character counts as its own segment. Whitespace runs collapse to a single
+/// low-cost segment. The result is clamped to at least 1 for non-empty input.
+pub fn estimate(text: &str) -> u64 {
+    if text.is_empty() {
