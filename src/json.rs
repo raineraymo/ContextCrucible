99c2a05b927beed31ea9cdec7e9654f33ba05b3e
silverhes <17355260+silverhes@users.noreply.github.com>
2016-10-24 11:57:00 +0700

    fix: correct secrets detection

--- src/json.rs
+++ src/json.rs
@@ -54,3 +54,13 @@ impl Json {
             Json::Array(items) => {
                 if items.is_empty() {
                     out.push_str("[]");
+                    return;
+                }
+                out.push_str("[\n");
+                for (i, item) in items.iter().enumerate() {
+                    push_indent(out, indent + 1);
+                    item.write_pretty(out, indent + 1);
+                    if i + 1 < items.len() {
+                        out.push(',');
+                    }
+                    out.push('\n');
