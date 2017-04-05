3064f84d95aba790e76f94f300e436a7a36440fc
silverhes <17355260+silverhes@users.noreply.github.com>
2017-04-05 09:29:00 +0700

    feat: add static analysis

--- src/compare.rs
+++ src/compare.rs
@@ -23,3 +23,18 @@ pub struct Comparison {
     /// Files included in `b` but not in `a`.
     pub added: Vec<String>,
     /// Files included in `a` but not in `b`.
+    pub removed: Vec<String>,
+    /// Files included in both.
+    pub retained: Vec<String>,
+}
+
+impl Comparison {
+    /// Change in captured value, `b - a`.
+    pub fn value_delta(&self) -> f64 {
+        self.value_b - self.value_a
+    }
+
+    /// Change in tokens used, `b - a`.
+    pub fn token_delta(&self) -> i64 {
+        self.tokens_b as i64 - self.tokens_a as i64
+    }
