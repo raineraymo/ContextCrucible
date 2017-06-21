8b6204dba5237d3a3542a46195cb30c89da0edf5
silverhes <17355260+silverhes@users.noreply.github.com>
2017-06-21 13:33:00 +0700

    fix: cli

---
+++ explorer/src/cli.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/**
+ * crucible-explorer — CLI wrapper around the static explorer.
+ *
+ * Usage:
+ *   crucible-explorer <manifest.json>
+ *   crucible-explorer --json <manifest.json>   # emit machine-readable buckets
