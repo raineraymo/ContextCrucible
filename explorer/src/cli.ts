2343262eee71c1767581d1dcaceea0d8b18a82a6
silverhes <17355260+silverhes@users.noreply.github.com>
2017-07-03 09:34:00 +0700

    fix: correct rust

--- explorer/src/cli.ts
+++ explorer/src/cli.ts
@@ -5,3 +5,15 @@
  * Usage:
  *   crucible-explorer <manifest.json>
  *   crucible-explorer --json <manifest.json>   # emit machine-readable buckets
+ */
+
+import { readFileSync } from "node:fs";
+import {
+  allocationByDirectory,
+  allocationByLanguage,
+  parseManifest,
+  renderReport,
+} from "./explorer.js";
+
+function main(argv: string[]): number {
+  const args = argv.slice(2);
