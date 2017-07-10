6c72f922b2fb127ad32e69e36706fa6c12a13ea7
silverhes <17355260+silverhes@users.noreply.github.com>
2017-07-10 11:47:00 +0700

    build: static analysis

--- explorer/src/explorer.test.ts
+++ explorer/src/explorer.test.ts
@@ -14,3 +14,20 @@ import {
   renderReport,
   type Manifest,
 } from "./explorer.js";
+
+function sampleManifest(): Manifest {
+  return {
+    summary: {
+      tool: "contextcrucible",
+      version: "0.4.0",
+      label: "test",
+      query: "budget solver",
+      budget_tokens: 2000,
+      tokens_used: 1000,
+      budget_utilization: 0.5,
+      captured_value: 100.0,
+      min_score: 0,
+      solver_method: "dp",
+      counts: {
+        included: 3,
+        excluded_secret: 1,
