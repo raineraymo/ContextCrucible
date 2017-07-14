5c76033ff142a5bd58bd8c8bc9d0192ec197dcb1
silverhes <17355260+silverhes@users.noreply.github.com>
2017-07-14 19:29:00 +0700

    fix: static analysis

---
+++ explorer/src/explorer.ts
@@ -0,0 +1,25 @@
+/**
+ * contextcrucible explorer — a dependency-free, static analyser for the JSON
+ * manifests produced by the `crucible` CLI.
+ *
+ * The explorer does not run the compiler; it *reads a manifest* and renders the
+ * pack allocation as a textual treemap / bar report. This keeps it fully
+ * offline (no network, no bundler) while giving a quick visual read on where a
+ * token budget went.
+ */
+
+/** A single file decision as recorded in a manifest. */
+export interface ManifestFile {
+  path: string;
+  decision: string;
+  tokens: number;
+  bytes: number;
+  grade: number;
+  language: string;
+  score_parts: {
+    path: number;
+    query: number;
+    import: number;
+    symbol: number;
+  };
+  fill_rank?: number;
