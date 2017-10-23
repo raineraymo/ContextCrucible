0d700875954f56e090783f390b97f7fbd7e2d6ad
silverhes <17355260+silverhes@users.noreply.github.com>
2017-10-23 14:00:00 +0700

    docs: expand rust

--- fixtures/sample-repo/main.py
+++ fixtures/sample-repo/main.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 """Entrypoint that drives a budget allocation for the sample repository."""
+
+from typing import Dict
+
+
+def allocate(total: int, weights: Dict[str, float]) -> Dict[str, int]:
+    """Distribute a total budget across weighted tasks."""
