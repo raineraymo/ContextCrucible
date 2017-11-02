e46812529a120bd3909683853264ffd286a5f421
silverhes <17355260+silverhes@users.noreply.github.com>
2017-11-02 18:12:00 +0700

    fix: correct context window

--- fixtures/sample-repo/src/budget_solver.rs
+++ fixtures/sample-repo/src/budget_solver.rs
@@ -1,3 +1,10 @@
 // A small budget solver fixture used by contextcrucible's integration tests.
 // It is intentionally relevant to the query "budget solver knapsack".
 
+use std::collections::HashMap;
+
+/// Solve a fractional budget allocation across weighted tasks.
+pub fn allocate_budget(total: u64, weights: &HashMap<String, f64>) -> HashMap<String, u64> {
+    let sum: f64 = weights.values().copied().sum();
+    let mut out = HashMap::new();
+    if sum <= 0.0 {
