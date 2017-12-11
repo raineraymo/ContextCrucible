f6d0ff52d5449d77992ffc874f32534f846d6ce2
silverhes <17355260+silverhes@users.noreply.github.com>
2017-12-11 13:40:00 +0700

    fix: correct code context

--- fixtures/sample-repo/src/strings.rs
+++ fixtures/sample-repo/src/strings.rs
@@ -6,3 +6,9 @@ pub fn human_size(bytes: u64) -> String {
     const UNITS: &[&str] = &["B", "KiB", "MiB", "GiB"];
     let mut value = bytes as f64;
     let mut unit = 0;
+    while value >= 1024.0 && unit < UNITS.len() - 1 {
+        value /= 1024.0;
+        unit += 1;
+    }
+    format!("{:.1} {}", value, UNITS[unit])
+}
