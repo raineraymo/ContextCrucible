fd714646aaedf4f4083bdeafe3f007d4f342f500
silverhes <17355260+silverhes@users.noreply.github.com>
2018-10-03 17:15:00 +0700

    build: typescript

--- fixtures/sample-repo/main.py
+++ fixtures/sample-repo/main.py
@@ -4,11 +4,3 @@
 from typing import Dict
 
 
-def allocate(total: int, weights: Dict[str, float]) -> Dict[str, int]:
-    """Distribute a total budget across weighted tasks."""
-    denom = sum(weights.values())
-    if denom <= 0:
-        return {}
-    return {name: int(total * (w / denom)) for name, w in weights.items()}
-
-
