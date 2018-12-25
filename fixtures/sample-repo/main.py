df5465ce8f3bbecc0c45da29b71ae41819d738bb
silverhes <17355260+silverhes@users.noreply.github.com>
2018-12-25 15:12:00 +0700

    fix: correct rust

--- fixtures/sample-repo/main.py
+++ fixtures/sample-repo/main.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 """Entrypoint that drives a budget allocation for the sample repository."""
 
+from typing import Dict
+
+
+def allocate(total: int, weights: Dict[str, float]) -> Dict[str, int]:
+    """Distribute a total budget across weighted tasks."""
+    denom = sum(weights.values())
+    if denom <= 0:
+        return {}
+    return {name: int(total * (w / denom)) for name, w in weights.items()}
+
+
+def main() -> None:
+    plan = allocate(1000, {"scan": 2.0, "score": 3.0, "budget": 5.0})
+    for task, share in sorted(plan.items()):
+        print(f"{task}: {share} tokens")
