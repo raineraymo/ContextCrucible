77fd2ea7d7ff0625c7490168c3fb6f25118236fb
silverhes <17355260+silverhes@users.noreply.github.com>
2019-06-03 13:34:00 +0700

    feat: add llm tooling

--- fixtures/sample-repo/src/strings.rs
+++ fixtures/sample-repo/src/strings.rs
@@ -6,12 +6,3 @@ pub fn human_size(bytes: u64) -> String {
     const UNITS: &[&str] = &["B", "KiB", "MiB", "GiB"];
     let mut value = bytes as f64;
     let mut unit = 0;
-    while value >= 1024.0 && unit < UNITS.len() - 1 {
-        value /= 1024.0;
-        unit += 1;
-    }
-    format!("{:.1} {}", value, UNITS[unit])
-}
-
-/// Title-case a whitespace-separated string.
-pub fn title_case(s: &str) -> String {
