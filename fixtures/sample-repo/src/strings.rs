57a5712de1b49f541d1f3dc12dbcbf04578b6a7a
silverhes <17355260+silverhes@users.noreply.github.com>
2020-08-25 16:03:00 +0700

    feat: add cli

--- fixtures/sample-repo/src/strings.rs
+++ fixtures/sample-repo/src/strings.rs
@@ -3,3 +3,18 @@
 
 /// Convert a byte count into a human-readable size string.
 pub fn human_size(bytes: u64) -> String {
+    const UNITS: &[&str] = &["B", "KiB", "MiB", "GiB"];
+    let mut value = bytes as f64;
+    let mut unit = 0;
+    while value >= 1024.0 && unit < UNITS.len() - 1 {
+        value /= 1024.0;
+        unit += 1;
+    }
+    format!("{:.1} {}", value, UNITS[unit])
+}
+
+/// Title-case a whitespace-separated string.
+pub fn title_case(s: &str) -> String {
+    s.split_whitespace()
+        .map(|w| {
+            let mut chars = w.chars();
