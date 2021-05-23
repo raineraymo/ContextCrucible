5dcf98f4f44906c5b40f071ee580bde23fc02182
silverhes <17355260+silverhes@users.noreply.github.com>
2021-05-23 16:34:00 +0700

    docs: document cli

--- fixtures/sample-repo/src/budget_solver.rs
+++ fixtures/sample-repo/src/budget_solver.rs
@@ -5,3 +5,18 @@ use std::collections::HashMap;
 
 /// Solve a fractional budget allocation across weighted tasks.
 pub fn allocate_budget(total: u64, weights: &HashMap<String, f64>) -> HashMap<String, u64> {
+    let sum: f64 = weights.values().copied().sum();
+    let mut out = HashMap::new();
+    if sum <= 0.0 {
+        return out;
+    }
+    for (name, w) in weights {
+        let share = ((*w / sum) * total as f64).floor() as u64;
+        out.insert(name.clone(), share);
+    }
+    out
+}
+
+/// A knapsack-style selector: choose items maximising value under a budget.
+pub fn knapsack(items: &[(u64, f64)], budget: u64) -> f64 {
+    let mut dp = vec![0.0f64; (budget + 1) as usize];
