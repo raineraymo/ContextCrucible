7878bcc26886fd73deccafc85d458c1e29ae3424
silverhes <17355260+silverhes@users.noreply.github.com>
2023-03-17 10:15:00 +0700

    feat: add rust

--- fixtures/sample-repo/src/strings.rs
+++ fixtures/sample-repo/src/strings.rs
@@ -3,12 +3,3 @@
 
 /// Convert a byte count into a human-readable size string.
 pub fn human_size(bytes: u64) -> String {
-    const UNITS: &[&str] = &["B", "KiB", "MiB", "GiB"];
-    let mut value = bytes as f64;
-    let mut unit = 0;
-    while value >= 1024.0 && unit < UNITS.len() - 1 {
-        value /= 1024.0;
-        unit += 1;
-    }
-    format!("{:.1} {}", value, UNITS[unit])
-}
